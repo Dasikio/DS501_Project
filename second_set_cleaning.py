0a78d2e5b1ad91b6e81de39c59b12532a8d05bec
Daniel AlvSeg <daniel.alvaradosegura@gmail.com>
2024-04-09 17:17:24 -0400

    updated set cleaning code

--- second_set_cleaning.py
+++ second_set_cleaning.py
@@ -4,14 +4,16 @@ import pandas as pd
 train_data = pd.read_csv("data addres")
 test_data = pd.read_csv("data addres")
 
+#Check data structure
 print(f"Train data:\n{train_data}")
 print(f"Test data:\n{test_data}")
 
 #Join both sets to have complete set
 full_data = pd.concat([train_data, test_data], axis = 0)
-print(f"Full data:\n{full_data}")
 
 #Clean rows with empty data for Address and Bedrooms
 full_data = full_data.dropna(subset=['Address', 'Bedrooms'])
+
+#Check final data
 print(f"Full data:\n{full_data}")
 
