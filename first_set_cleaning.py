5111042c3a29170370429d2152cbd2fa9045408f
Daniel AlvSeg <daniel.alvaradosegura@gmail.com>
2024-04-09 17:33:54 -0400

    Updated content of files

--- first_set_cleaning.py
+++ first_set_cleaning.py
@@ -1,7 +1,7 @@
 import pandas as pd
 
 #Retrieve data from csv
-coordinate_based_data = pd.read_csv("C:/Users/danie/Desktop/UK_Code/DS501/Project/California_House_Information/California_House_Info.csv")
+coordinate_based_data = pd.read_csv("file_address")
 
 #Clean rows with empty data for Address and Bedrooms
 cleaned_coordinate_based_data = coordinate_based_data.dropna(subset=['longitude', 'latitude'])
