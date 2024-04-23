dc9a85e12246958d5b3ef9c35d7062207d6487e4
Daniel AlvSeg <daniel.alvaradosegura@gmail.com>
2024-04-22 21:03:54 -0400

    update to first_set_cleaning

--- first_set_cleaning.py
+++ first_set_cleaning.py
@@ -2,7 +2,7 @@ import pandas as pd
 import requests
 import time
 
-
+# Function to get county from geocode
 def get_county(latitude, longitude):
     try:
         geocode_api = f"https://geocoding.geo.census.gov/geocoder/geographies/coordinates?x={longitude}&y={latitude}&benchmark=Public_AR_Census2020&vintage=Census2020_Census2020&layers=6&format=json"
@@ -15,6 +15,7 @@ def get_county(latitude, longitude):
         print(f"Error occurred for coordinates ({longitude}, {latitude}): {str(e)}")
         return None
 
+#Function to process a dataSet
 def complete_set1(file_path):
     coordinate_based_data = pd.read_csv(file_path)
     cleaned_coordinate_based_data = coordinate_based_data.dropna(subset=['longitude', 'latitude'])
@@ -29,20 +30,27 @@ def complete_set1(file_path):
             for col in avg_values.index:
                 filtered_data.loc[(filtered_data['longitude'] == coords[0]) &
                                   (filtered_data['latitude'] == coords[1]), col] = avg_values[col]
-
     filtered_data.drop_duplicates(subset=['longitude', 'latitude'], keep='first', inplace=True)
 
     return filtered_data
 
+#Function to filter data from wanted counties
 def county_filter(filtered_data, county_names):
-    filtered_data['county'] = filtered_data.apply(lambda row: get_county(row['latitude'], row['longitude']), axis=1)
-    
-    indices_to_drop = []
+    # Iterate over each county code in the 'county' column
+    indices_to_drop = []  # List to store indices of rows to drop
     for index, county_code in filtered_data['county'].items():
-        if county_code not in county_names:
+        # Check if the county code exists in the dictionary keys
+        if county_code in county_names:
+            # Replace the county code with the corresponding county name
+            filtered_data.at[index, 'county'] = county_names[county_code]
+        else:
+            # Add the index to the list of indices to drop
             indices_to_drop.append(index)
-    filtered_data.drop(indices_to_drop, inplace=True)
+
+        # Drop rows with indices from the list and round values
+        filtered_data.drop(indices_to_drop, inplace=True)
     return filtered_data
+
 
 def main(file_path, county_names):
     cleaned_dataset1 = complete_set1(file_path)
@@ -50,7 +58,10 @@ def main(file_path, county_names):
     return cleaned_dataset1
 
 if __name__ == "__main__":
-    file_path = "localpath/California_House_Info.csv"
+    #File path to dataSet
+    file_path = "local_path/California_House_Info.csv"
+    
+    # Define County codes wanted
     county_names = {
         '037': 'Los Angeles',
         '073': 'San Diego',
@@ -58,6 +69,7 @@ if __name__ == "__main__":
         '065': 'Riverside',
         '071': 'San Bernardino'
     }
+    #Load and Clean data for merging
     cleaned_dataset1 = main(file_path, county_names)
     cleaned_dataset1.to_csv('cleaned_dataset1.csv', index=False)
     print(cleaned_dataset1)
